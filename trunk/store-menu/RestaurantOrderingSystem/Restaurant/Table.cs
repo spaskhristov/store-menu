424df9a249efc0f0da9bfa65f70dd587c3bd876a
bgpa4o@gmail.com <bgpa4o@gmail.com@4a1c5792-e898-37e7-dea2-dbe9b76abe24>
2013-11-25 11:35:26 +0000

    changes Login

---
+++ trunk/store-menu/RestaurantOrderingSystem/Restaurant/Table.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantOrderingSystem
+{
+    class Table
+    {
+        // Properties
+        public Zone Zone { get; set; }
+        public List<Client> Clients { get; set; }
+    }
+}
